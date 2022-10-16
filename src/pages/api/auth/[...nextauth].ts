14ac58ea0af49e662b9b4d17ac00343e8ce38d9c
Makon <makon@hey.com>
2022-10-16 14:51:37 -0400

    remove email sign in

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,4 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import EmailProvider from "next-auth/providers/email";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
@@ -29,10 +28,6 @@ export const authOptions: NextAuthOptions = {
           email: profile.email,
         };
       },
-    }),
-    EmailProvider({
-      server: env.EMAIL_SERVER,
-      from: env.EMAIL_FROM,
     }),
   ],
 };
