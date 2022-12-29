e6bcf47027a695178e64c8fc3f71bc4a279000fe
Makon <makon@hey.com>
2022-12-29 01:26:16 -0500

    add user location and sort places by distance

---
+++ src/hooks/useUserLocation.ts
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const useUserLocation = ({
+  onUserLocationEnabled,
+}: {
+  onUserLocationEnabled: () => void;
+}) => {
+  const [userLocation, setUserLocation] =
+    React.useState<GeolocationPosition | null>(null);
+  const [userLocationError, setUserLocationError] =
+    React.useState<GeolocationPositionError | null>(null);
+
+  const getUserLocation = React.useCallback(() => {
+    navigator.geolocation.getCurrentPosition(
+      setUserLocation,
+      setUserLocationError,
+      {
+        maximumAge: Infinity,
+        timeout: 5000,
+        enableHighAccuracy: true, // doesn't work consisently wihtout this
+      }
+    );
+    onUserLocationEnabled();
+  }, [onUserLocationEnabled]);
+
+  React.useEffect(() => {
+    navigator.permissions
+      .query({ name: "geolocation" })
+      .then((permissionStatus) => {
+        if (permissionStatus.state === "granted") {
+          getUserLocation();
+        }
+      });
+  }, [getUserLocation]);
+  return { userLocation, userLocationError, getUserLocation };
+};
