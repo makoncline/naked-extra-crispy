16da884bbf3dc2c0302635f51c062e2caba9ac8b
Makon <makon@hey.com>
2023-06-04 12:48:18 -0400

    change nav logo and hero photo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,77 +3,71 @@ import { siteConfig } from "../siteConfig";
 import { row } from "../styles/utils";
 import Image from "next/image";
 import wings from "../../public/wings.webp";
+import { LogoSVG } from "./LogoSvg";
 
 export const Hero = () => {
   const { title, description } = siteConfig;
   return (
-    <div
-      css={`
-        display: grid;
-        align-items: center;
-        grid-template-columns: 1fr 1fr;
-      `}
-    >
-      <section
+    <div>
+      <div
         css={`
           display: grid;
-          gap: var(--size-5);
+          align-items: center;
+          grid-template-columns: 1fr 1fr;
         `}
       >
-        <h1
+        <section
           css={`
             display: grid;
-            grid-template-columns: max-content;
-            color: var(--text);
-            line-height: var(--font-lineheight-0);
-            font-size: var(--font-size-fluid-2);
-            & > div:last-child {
-              color: var(--brand-color);
-              font-size: var(--font-size-fluid-1);
-            }
+            gap: var(--size-5);
           `}
         >
-          <div>{title}</div>
-          <div>{description}</div>
-        </h1>
-        <p
+          <h1
+            css={`
+              display: grid;
+              grid-template-columns: max-content;
+              color: var(--text);
+              line-height: var(--font-lineheight-0);
+              font-size: var(--font-size-fluid-2);
+              & > div:last-child {
+                color: var(--brand-color);
+                font-size: var(--font-size-fluid-1);
+              }
+            `}
+          >
+            <div>{title}</div>
+            <div>{description}</div>
+          </h1>
+          <p
+            css={`
+              margin-block-end: var(--size-3);
+              font-size: var(--font-size-fluid-0);
+            `}
+          >
+            Naked Extra Crispy. It's how we like our wings. Good wings are
+            crispy, but never breaded. They're tossed in buffalo sauce and come
+            with a dip. Housemade sauces are key. We're not a wing spot, but we
+            do wings right.
+          </p>
+        </section>
+        <div
           css={`
-            margin-block-end: var(--size-3);
-            font-size: var(--font-size-fluid-0);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            width: 100%;
+            padding: var(--space-sm);
           `}
         >
-          Naked Extra Crispy. It's how we like our wings. Good wings are crispy,
-          but never breaded. They're tossed in buffalo sauce and come with a
-          dip. Housemade sauces are key. We're not a wing spot, but we do wings
-          right.
-        </p>
-        <div>
-          <Link href="#results">
-            <button>Show me the wings</button>
-          </Link>
+          <LogoSVG />
         </div>
-      </section>
-      <picture
-        css={`
-          ${row}
-          align-items: center;
-          position: relative;
-          align-self: stretch;
-          & > img {
-            block-size: 100%;
-            object-fit: cover;
-          }
-        `}
-      >
-        <Image
-          src={wings}
-          alt="a wing doodle"
-          width={400}
-          height={400}
-          priority
-          placeholder="blur"
-        />
-      </picture>
+      </div>
+      <div>
+        <Link href="#results">
+          <button>Show me the wings</button>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,43 +1,66 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { col } from "../styles/utils";
-import logo from "../../public/nxcLogo.webp";
+import { col, row } from "../styles/utils";
+import logo from "../../public/logo.png";
 import { Loading } from "./Loading";
+import { DrumSvg } from "./DrumSvg";
+import { useRouter } from "next/router";
 
 export const Navigation = () => {
+  const router = useRouter();
+  const showLogoText = router.pathname !== "/";
   const { data: session, status } = useSession();
   const isLoading = status === "loading";
   return (
     <div
       css={`
         display: grid;
-        grid-template-columns: 1fr 1fr;
+        grid-template-columns: auto 1fr;
         align-items: center;
         padding-top: var(--size-2);
         width: 100%;
       `}
     >
-      <div
-        css={`
-          font-weight: var(--font-weight-9);
-        `}
-      >
-        <Link href="/" passHref>
-          <a>
-            <Image
-              src={logo}
-              width={200}
-              height={100}
-              objectFit="cover"
-              alt="Naked Extra Crispy logo"
-              priority
-              placeholder="blur"
-            />
-          </a>
-        </Link>
-      </div>
-
+      <Link href="/" passHref>
+        <a>
+          <div
+            css={`
+              ${row}
+              align-items: center;
+            `}
+          >
+            <DrumSvg />
+            {showLogoText && (
+              <div
+                css={`
+                  ${col}
+                  gap: 0;
+                  color: var(--text-2);
+                  line-height: 1;
+                `}
+              >
+                <span
+                  css={`
+                    font-size: var(--font-size-4);
+                    font-weight: var(--font-weight-9);
+                  `}
+                >
+                  Naked
+                </span>
+                <span
+                  css={`
+                    font-size: var(--font-size-0);
+                    font-weight: var(--font-weight-8);
+                  `}
+                >
+                  Extra Crispy
+                </span>
+              </div>
+            )}
+          </div>
+        </a>
+      </Link>
       <div
         css={`
           justify-self: end;
